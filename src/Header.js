270adff050937e6c8e473e04e58e0a0a9e4d3656
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-05-11 18:04:54 -0700

    finished lab 01

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor() {
+  super();
+}
+
+render() {
+  return(
+    <div>
+    <header>Horned Beasts</header>
+    </div>
+    )
+  }
+}
+
+export default Header;
